fe9ac264fe0bb7ac94fa99b2f417144c83bc7e5f
ryzmdn <ryzmdn@gmail.com>
2026-06-07 16:25:44 +0700

    feat: added digital applications and software section on the digitization page

--- app/digitalisasi/page.tsx
+++ app/digitalisasi/page.tsx
@@ -1,45 +1,88 @@
 const collections = [
   {
     name: "Women's",
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    imageAlt: 'Woman wearing an off-white cotton t-shirt.',
+    href: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Woman wearing an off-white cotton t-shirt.",
   },
   {
     name: "Men's",
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    imageAlt: 'Man wearing a charcoal gray cotton t-shirt.',
+    href: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Man wearing a charcoal gray cotton t-shirt.",
   },
   {
-    name: 'Desk Accessories',
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    imageAlt: 'Person sitting at a wooden desk with paper note organizer, pencil and tablet.',
+    name: "Desk Accessories",
+    href: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt:
+      "Person sitting at a wooden desk with paper note organizer, pencil and tablet.",
   },
-]
+];
+
+const people = [
+  {
+    name: "Leonard Krasner",
+    role: "Senior Designer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Floyd Miles",
+    role: "Principal Designer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Emily Selman",
+    role: "VP, User Experience",
+    imageUrl:
+      "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Kristin Watson",
+    role: "VP, Human Resources",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Emma Dorsey",
+    role: "Senior Developer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Alicia Bell",
+    role: "Junior Copywriter",
+    imageUrl:
+      "https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    xUrl: "#",
+    linkedinUrl: "#",
+  },
+];
 
 export default function DigitalisasiPage() {
   return (
-    <div className="relative bg-white">
-      {/* Background image and overlap */}
-      <div aria-hidden="true" className="absolute inset-0 hidden sm:flex sm:flex-col">
-        <div className="relative w-full flex-1 bg-gray-800">
-          <div className="absolute inset-0 overflow-hidden">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="size-full object-cover"
-            />
-          </div>
-          <div className="absolute inset-0 bg-gray-900 opacity-50" />
-        </div>
-        <div className="h-32 w-full bg-white md:h-40 lg:h-48" />
-      </div>
-
-      <div className="relative mx-auto max-w-3xl px-4 pb-96 text-center sm:px-6 sm:pb-0 lg:px-8">
+    <>
+      <div className="relative bg-white">
         {/* Background image and overlap */}
-        <div aria-hidden="true" className="absolute inset-0 flex flex-col sm:hidden">
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 hidden sm:flex sm:flex-col"
+        >
           <div className="relative w-full flex-1 bg-gray-800">
             <div className="absolute inset-0 overflow-hidden">
               <img
@@ -50,54 +93,226 @@ export default function DigitalisasiPage() {
             </div>
             <div className="absolute inset-0 bg-gray-900 opacity-50" />
           </div>
-          <div className="h-48 w-full bg-white" />
+          <div className="h-32 w-full bg-white md:h-40 lg:h-48" />
         </div>
-        <div className="relative py-32">
-          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">Mid-Season Sale</h1>
-          <div className="mt-4 sm:mt-6">
+
+        <div className="relative mx-auto max-w-3xl px-4 pb-96 text-center sm:px-6 sm:pb-0 lg:px-8">
+          {/* Background image and overlap */}
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 flex flex-col sm:hidden"
+          >
+            <div className="relative w-full flex-1 bg-gray-800">
+              <div className="absolute inset-0 overflow-hidden">
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  className="size-full object-cover"
+                />
+              </div>
+              <div className="absolute inset-0 bg-gray-900 opacity-50" />
+            </div>
+            <div className="h-48 w-full bg-white" />
+          </div>
+          <div className="relative py-32">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+              Mid-Season Sale
+            </h1>
+            <div className="mt-4 sm:mt-6">
+              <a
+                href="#"
+                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 font-medium text-white hover:bg-indigo-700"
+              >
+                Shop Collection
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <section
+          aria-labelledby="collection-heading"
+          className="relative -mt-96 sm:mt-0"
+        >
+          <h2 id="collection-heading" className="sr-only">
+            Collections
+          </h2>
+          <div className="mx-auto grid max-w-md grid-cols-1 gap-y-6 px-4 sm:max-w-7xl sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 sm:px-6 lg:gap-x-8 lg:px-8">
+            {collections.map((collection) => (
+              <div
+                key={collection.name}
+                className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-4/5 sm:h-auto"
+              >
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-0 overflow-hidden rounded-lg"
+                >
+                  <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                    <img
+                      alt={collection.imageAlt}
+                      src={collection.imageSrc}
+                      className="size-full object-cover"
+                    />
+                  </div>
+                  <div className="absolute inset-0 bg-linear-to-b from-transparent to-black opacity-50" />
+                </div>
+                <div className="absolute inset-0 flex items-end rounded-lg p-6">
+                  <div>
+                    <p aria-hidden="true" className="text-sm text-white">
+                      Shop the collection
+                    </p>
+                    <h3 className="mt-1 font-semibold text-white">
+                      <a href={collection.href}>
+                        <span className="absolute inset-0" />
+                        {collection.name}
+                      </a>
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+
+      <div className="mx-auto max-w-7xl w-full px-6 py-20 text-center lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+            Meet our team
+          </h2>
+          <p className="mt-6 text-lg/8 text-gray-600">
+            We’re a dynamic group of individuals who are passionate about what
+            we do and dedicated to delivering the best results for our clients.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto grid max-w-2xl grid-cols-1 gap-6 mt-12 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+        >
+          {people.map((person) => (
+            <li
+              key={person.name}
+              className="rounded-2xl bg-gray-100 px-8 py-10"
+            >
+              <img
+                alt=""
+                src={person.imageUrl}
+                className="mx-auto size-48 rounded-full outline-1 -outline-offset-1 outline-black/5 md:size-56"
+              />
+              <h3 className="mt-6 text-base/7 font-semibold tracking-tight text-gray-900">
+                {person.name}
+              </h3>
+              <p className="text-sm/6 text-gray-600">{person.role}</p>
+              <div role="list" className="flex justify-center items-center gap-x-3">
+                <a href="#">
+                  <img src="https://www.svgrepo.com/show/303118/google-play-download-android-app-logo.svg" alt="Google Play" className="w-auto h-28" />
+                </a>
+                <a href="#">
+                  <img src="https://www.svgrepo.com/show/303128/download-on-the-app-store-apple-logo.svg" alt="Google Play" className="w-auto h-32" />
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mx-auto max-w-2xl w-full px-4 py-5 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="relative overflow-hidden rounded-lg lg:h-96">
+          <div className="absolute inset-0">
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1533022139390-e31c488d69e2?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="size-full object-cover"
+            />
+          </div>
+          <div aria-hidden="true" className="relative h-96 w-full lg:hidden" />
+          <div aria-hidden="true" className="relative h-32 w-full lg:hidden" />
+          <div className="absolute inset-x-0 bottom-0 rounded-br-lg rounded-bl-lg bg-black/75 p-6 backdrop-blur-sm backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start lg:rounded-tl-lg lg:rounded-br-none">
+            <div>
+              <h2 className="text-xl font-bold text-white">
+                Workspace Collection
+              </h2>
+              <p className="mt-1 text-sm text-gray-300">
+                Upgrade your desk with objects that keep you organized and
+                clear-minded.
+              </p>
+            </div>
             <a
               href="#"
-              className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 font-medium text-white hover:bg-indigo-700"
+              className="mt-6 flex shrink-0 items-center justify-center rounded-md border border-white/25 px-4 py-3 text-base font-medium text-white hover:bg-white/10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full"
             >
-              Shop Collection
+              View the collection
             </a>
           </div>
         </div>
       </div>
 
-      <section aria-labelledby="collection-heading" className="relative -mt-96 sm:mt-0">
-        <h2 id="collection-heading" className="sr-only">
-          Collections
-        </h2>
-        <div className="mx-auto grid max-w-md grid-cols-1 gap-y-6 px-4 sm:max-w-7xl sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 sm:px-6 lg:gap-x-8 lg:px-8">
-          {collections.map((collection) => (
-            <div
-              key={collection.name}
-              className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-4/5 sm:h-auto"
-            >
-              <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
-                <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
-                  <img alt={collection.imageAlt} src={collection.imageSrc} className="size-full object-cover" />
-                </div>
-                <div className="absolute inset-0 bg-linear-to-b from-transparent to-black opacity-50" />
-              </div>
-              <div className="absolute inset-0 flex items-end rounded-lg p-6">
-                <div>
-                  <p aria-hidden="true" className="text-sm text-white">
-                    Shop the collection
-                  </p>
-                  <h3 className="mt-1 font-semibold text-white">
-                    <a href={collection.href}>
-                      <span className="absolute inset-0" />
-                      {collection.name}
-                    </a>
-                  </h3>
-                </div>
+      <div className="mx-auto max-w-2xl w-full px-4 py-5 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+          <div className="group relative aspect-2/1 overflow-hidden rounded-lg sm:row-span-2 sm:aspect-square">
+            <img
+              alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
+              src="https://images.unsplash.com/photo-1601972599720-36938d4ecd31?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="absolute size-full object-cover group-hover:opacity-75"
+            />
+            <div aria-hidden="true" className="absolute inset-0 bg-linear-to-b from-transparent to-black opacity-50" />
+            <div className="absolute inset-0 flex items-end p-6">
+              <div>
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    New Arrivals
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  Shop now
+                </p>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="group relative aspect-2/1 overflow-hidden rounded-lg sm:aspect-auto">
+            <img
+              alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters."
+              src="https://images.unsplash.com/photo-1601972599720-36938d4ecd31?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="absolute size-full object-cover group-hover:opacity-75"
+            />
+            <div aria-hidden="true" className="absolute inset-0 bg-linear-to-b from-transparent to-black opacity-50" />
+            <div className="absolute inset-0 flex items-end p-6">
+              <div>
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    Accessories
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  Shop now
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="group relative aspect-2/1 overflow-hidden rounded-lg sm:aspect-auto">
+            <img
+              alt="Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk."
+              src="https://images.unsplash.com/photo-1601972599720-36938d4ecd31?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="absolute size-full object-cover group-hover:opacity-75"
+            />
+            <div aria-hidden="true" className="absolute inset-0 bg-linear-to-b from-transparent to-black opacity-50" />
+            <div className="absolute inset-0 flex items-end p-6">
+              <div>
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    Workspace
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  Shop now
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
